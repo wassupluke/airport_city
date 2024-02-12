b2056070feb7a35e45a0de04d54d25584da7033c
ud4838tv <ud4838tv@go.minnstate.edu>
2024-02-12 10:16:45 -0600

    Added a auto exit for unwanted menus
    Made for 1366 resolution. 1920 resolution tested and failed. Feel free to make changes to (duration it searches for coins, planes, and other items) for your own personal needs. Asset files also may need to be changed.

--- coins.py
+++ coins.py
@@ -19,30 +19,24 @@ from time import sleep
 
 def main():
     find_focus()
-    print('::::::::::::')
-    find_suitcase()
-    print('::::::::::::')
+    # find_suitcase()
+    neighbor()
     find_coin()
-    print('::::::::::::')
-    send_planes()
-    print('::::::::::::')
-    guest_planes()
-    print('::::::::::::')
-    objective_complete()
-    print('::::::::::::')
-    collections()
-    print('::::::::::::')
-    congratulations()
-    print('::::::::::::')
     end()
-    print('::::::::::::')
+    send_planes()
+    end()
+    fix_planes()
+    end()
+    guest_planes()
+    end()
+    collections()
+    congratulations()
+    end()
 
 
 def find_focus():
-    print('running find_focus')
-    title = '🛩 Version 8.31.24 | SupportID: 20KPQFMVY ‎- Airport City'
-    # VERY IMPORTANT NOTE:
-    '''Title may need revised if game version updates
+    title = '🛩 Version 8.31.24 | SupportID: 20NNHCHM5 ‎- Airport City GI'
+    '''NOTE: Title may need revised if game version updates
     use the list_window_names function to see titles of all
     open windows'''
     hwnd = win32gui.FindWindow(None, title)  # locates game
@@ -50,13 +44,12 @@ def find_focus():
 
 
 def find_suitcase():
-    print('running find_suitcase')
     pngs = ['suitcase.png',
             'suitcase_sm.png',
             'suitcase_sm2.png',
             'suitcase_sm3.png']
     for png in pngs:
-        suitcase = pyautogui.locateOnScreen(png, confidence=0.45)
+        suitcase = pyautogui.locateOnScreen(png, confidence=0.8)
         if suitcase is not None:
             pyautogui.moveTo(suitcase[0] + 8, suitcase[1] + 8)
             click()
@@ -69,38 +62,50 @@ def find_suitcase():
 
 
 def find_coin():
-    print('running find_coin')
     pngs = ['coin.png',
             'coin_sm.png',
-            'coin_sm1.png']
+            'coin_sm1.png',
+            'coin_sm2.png']
     for png in pngs:
-        for _ in range(3):
+        for _ in range(4):
             coin = pyautogui.locateOnScreen(png, confidence=0.5)
             if coin is not None:
                 pyautogui.moveTo(coin[0] + 8, coin[1] + 8)
                 click()
-                sleep(0.3)
+                sleep(0.1)
 
 
 def send_planes():
-    print('running send_planes')
-    plane = pyautogui.locateOnScreen('ready_plane.png', confidence=0.65)
-    if plane is not None:  # checks that a plane is ready to send
-        pyautogui.moveTo(plane[0] + 7, plane[1] + 7)
-        click()
-        # FUTURE FEATURE: compares fuel/pass required to available supply
-        depart = pyautogui.locateOnScreen('depart.png', confidence=0.8)
-        if depart is not None:  # checks that plane can depart
-            pyautogui.moveTo(depart[0] + 30, depart[1] + 30)
+    pngs = ['ready_plane.png',
+            'ready_plane2.png',
+            'ready_plane3.png',
+            'ready_plane4.png',
+            'ready_plane5.png',
+            'ready_plane6.png']
+    for png in pngs:
+        sleep(0.1)
+        plane = pyautogui.locateOnScreen(png, confidence=0.65)
+        if plane is not None:
+            sleep(0.1)
+            pyautogui.moveTo(plane[0] + 7, plane[1] + 7)
             click()
-            bad_reqs = pyautogui.locateOnScreen('departure_reqs_not_met.png',
-                                                confidence=0.7)
-            if bad_reqs is not None:
-                pyautogui.press('esc')
-                sleep(0.2)
-                pyautogui.press('esc')
+            depart = pyautogui.locateOnScreen('depart.png', confidence=0.8)
+            if depart is not None:
+                pyautogui.moveTo(depart[0] + 30, depart[1] + 30)
+                click()
+                bad_reqs = pyautogui.locateOnScreen('departure_reqs_not_met.png',
+                                                    confidence=0.7)
+                if bad_reqs is not None:
+                    pyautogui.press('esc')
+                    sleep(0.2)
+                    pyautogui.press('esc')
     # COLLECT PILOT CAPS
-    caps = ['pilot_cap.png', 'pilot_cap2.png', 'pilot_cap3.png']
+    caps = ['pilot_cap.png',
+            'pilot_cap2.png',
+            'pilot_cap3.png',
+            'pilot_cap4.png',
+            'pilot_cap5.png',
+            'pilot_cap6.png']
     for c in caps:
         cap = pyautogui.locateOnScreen(c, confidence=0.75)
         if cap is not None:  # checks for pilot caps on landed planes
@@ -109,7 +114,6 @@ def send_planes():
 
 
 def guest_planes():
-    print('running guest_planes')
     # LAND GUEST PLANES
     tower = pyautogui.locateOnScreen('guest_planes_tower.png', confidence=0.9)
     if tower is not None:  # checks that guest tower shows
@@ -121,91 +125,46 @@ def guest_planes():
             bad_reqs = pyautogui.locateOnScreen('landing_reqs_not_met.png',
                                                 confidence=0.8)
             if reqs is not None:  # checks that planes can land
-                pyautogui.moveTo(reqs[0] + 250, reqs[1] + 15)
+                pyautogui.moveTo(reqs[0] + 350, reqs[1] + 15)
                 click()
                 attn = pyautogui.locateOnScreen('land_planes_in_queue.png',
                                                 confidence=0.8)
-                if attn is not None:  # checks for full landing queue
+                """if attn is not None:  # checks for full landing queue
                     pyautogui.press('esc')
             elif bad_reqs is not None:  # checks for unmet landing reqs
-                pyautogui.moveTo(bad_reqs[0] + 250, bad_reqs[1] + 75)
-                click()
+                pyautogui.moveTo(bad_reqs[0] + 350, bad_reqs[1] + 75)
+                click()"""
             else:
                 pyautogui.press('esc', presses=2)
                 break
 
 
-def objective_complete():
-    print('looking for completed objectives')
-    marks = ['quests_icon.png', 'checkmark.png']
-    for mark in marks:
-        check = pyautogui.locateOnScreen(mark, confidence=0.9)
-        if check is not None:
-            pyautogui.moveTo(check[0], check[1] + 10)
-            click()
-            sleep(0.5)
-            for _ in range(2):
-                take = pyautogui.locateOnScreen('take.png', confidence=0.6)
-                if take is not None:
-                    pyautogui.moveTo(take[0] + 60, take[1] + 20)
-                    click()
-                    congratulations()
-
-
 def collections():
-    print('running collections')
     icon = pyautogui.locateOnScreen('collections_icon.png', confidence=0.95)
     if icon is not None:
         pyautogui.moveTo(icon)
+        sleep(1)
         click()
         sleep(0.5)
-        buttons = ['collect.png', 'collect2.png']
+        buttons = ['collect.png',
+                    'collect2.png',
+                    'collect3.png']
         for button in buttons:
-            collect = pyautogui.locateOnScreen(button, confidence=0.9)
-            if collect is not None:
+             collect = pyautogui.locateOnScreen(button, confidence=0.9)
+             if collect is not None:
                 pyautogui.moveTo(collect[0] + 8, collect[1] + 8)
                 click()
+                sleep(0.8)
+                pyautogui.press('esc')
 
 
 def congratulations():
-    print('looking for congratulations')
     congrats = pyautogui.locateOnScreen('congratulations.png', confidence=0.9)
     if congrats is not None:
         pyautogui.press('esc')
 
 
-def end():
-    print('running end')
-    pyautogui.press('esc')
-    confirm_quit = pyautogui.locateOnScreen('quit.png', confidence=0.8)
-    if confirm_quit is not None:
-        pyautogui.press('esc')
-    
-
-def cargo_container():
-    print('running cargo_container')
-    cargo = pyautogui.locateOnScreen('cargo_container_icon.png',
-                                     confidence=0.9)
-    if cargo is not None:
-        pyautogui.moveTo(cargo[0] + 8, cargo[1] + 8)
-        click()
-        container = pyautogui.locateOnScreen('cargo_container.png',
-                                             confidence=0.8)
-        if container is not None:
-            pyautogui.moveTo(container)
-            click()
-            take = pyautogui.locateOnScreen('take_cargo.png', confidence=0.8)
-            if take is not None:
-                pyautogui.moveTo(take[0] + 40, take[1] + 10)
-                click()
-                sleep(5)
-                congratulations()
-                sleep(0.1)
-                pyautogui.press('esc')
-
-
 def click():
-    print('click')
     pydirectinput.mouseDown()  # clicks the mouse
     sleep(0.01)
     pydirectinput.mouseUp()
@@ -218,9 +177,45 @@ def list_window_names():  # Prints names of all active windows
     win32gui.EnumWindows(winEnumHandler, None)
 
 
+def end():
+    pyautogui.press('esc')
+    comfirm_end = pyautogui.locateOnScreen('quit.png', confidence=0.8)
+    if comfirm_end is not None:
+        pyautogui.press('esc')
+
+
+def neighbor():
+    neighbor = pyautogui.locateOnScreen('neighbor.png', confidence=0.8)
+    if neighbor is not None:
+        pyautogui.moveTo(neighbor[0] + 10, neighbor[1] + 10)
+        click()
+
+
+def fix_planes():
+    pngs = ['fix_plane1.png',
+            'fix_plane2.png',
+            'fix_plane3.png',
+            'fix_plane4.png',
+            'fix_plane5.png']
+    for png in pngs:
+        fix = pyautogui.locateOnScreen(png, confidence=0.65)
+        if fix is not None:
+            pyautogui.moveTo(fix[0] + 7, fix[1] + 7)
+            click()
+            sleep(1)
+            repair = pyautogui.locateOnScreen('repair.png', confidence=0.8)
+            if repair is not None:
+                pyautogui.moveTo(repair[0] + 8, repair[1] + 8)
+                click()
+                sleep(1)
+                speedup = pyautogui.locateOnScreen('speedup.png', confidence=0.8)
+                if speedup is not None:
+                    pyautogui.moveTo(speedup[0] + 8, speedup[1] + 8)
+                    click()
+
+
 pyautogui.FAILSAFE = True
 # fail-safe triggers from mouse moving to the top left corner of the screen
 while True:
     for _ in range(10):
         main()
-    cargo_container()
